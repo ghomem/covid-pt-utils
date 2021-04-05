a2c242be68febbf8bcaf36b8be02d9ec94bbe962
Diogo Mera <diogo.mera@gmail.com>
2021-04-05 23:10:56 +0100

    V. 1

--- fetch-dssg-data.py
+++ fetch-dssg-data.py
@@ -4,90 +4,33 @@ Created on Sun Mar 21 17:11:21 2021
 
 @author: Diogo
 """
-
-
-
 # Libraries needed for the tutorial
 
 import pandas as pd
 import requests
 import io
 import datetime
-    
-# Downloading the csv file from your GitHub account
+import os
 
-
-
-#df = pd.read_csv('data.csv')
-
-#print(df.head()) 
-
-
-csv_url = "https://raw.githubusercontent.com/dssg-pt/covid19pt-data/master/data.csv" # Make sure the url is the raw version of the file on GitHub
-
-#csv_url = "https://raw.githubusercontent.com/dssg-pt/covid19pt-data/master/amostras.csv" # Make sure the url is the raw version of the file on GitHub
-
-
-
-#csv_url="https://raw.githubusercontent.com/dssg-pt/covid19pt-data/master/vacinas.csv"
-
-
-
-#https://raw.githubusercontent.com/dssg-pt/covid19pt-data/master/data.csv
-#https://github.com/dssg-pt/dados-SICOeVM/blob/master/mortalidade.csv
-#https://raw.githubusercontent.com/dssg-pt/covid19pt-data/master/vacinas.csv
-
+csv_url = [ "https://raw.githubusercontent.com/dssg-pt/covid19pt-data/master/data.csv",
+            "https://raw.githubusercontent.com/dssg-pt/covid19pt-data/master/amostras.csv,",
+            "https://raw.githubusercontent.com/dssg-pt/covid19pt-data/master/mortalidade.csv",
+            "https://raw.githubusercontent.com/dssg-pt/covid19pt-data/master/vacinas.csv",
+          ]
 
 time_path = str(datetime.date.today())
 
-name_path='data_'
+# Downloading the csv file + giving name
 
-file_name_path=name_path+time_path+'.csv'
-
-req = requests.get(csv_url)
-url_content = req.content
-csv_file = open(file_name_path, 'wb')
-
-csv_file.write(url_content)
-csv_file.close()
-
-
-df = pd.read_csv('downloaded.csv')
-
-
-print (df)
-
-print(file_name_path)
-
-
-
-
-
-x = datetime.date.today()
-
-
-
-print(x)
-print(x.strftime("%A"))
-
-
-
-#print("Hello new World")
-
-#url = "https://github.com/dssg-pt/covid19pt-data/blob/master/data.csv" # Make sure the url is the raw version of the file on GitHub
-#download = requests.get(url).content
-
-#print(download)
-
-# # Reading the downloaded content and turning it into a pandas dataframe
-
-
-#df = pd.read_csv(io.StringIO(download.decode('utf-8')))
-
-#  # Printing out the first 5 rows of the dataframe
-
-#! pwd
-
-# df = pd.read_csv(download)
-
-# print (df.head())
+for url in csv_url:
+    print('downloading ' + url)
+    name_path_csv = os.path.basename(url)
+    name_path_split = name_path_csv.split('.')
+    name_path = name_path_split[0]
+    file_name_path = name_path + '-' + time_path + '.csv'
+    print('saving ' + file_name_path)
+    req = requests.get(url)
+    url_content = req.content
+    csv_file = open(file_name_path, 'wb')
+    csv_file.write(url_content)
+    csv_file.close()
