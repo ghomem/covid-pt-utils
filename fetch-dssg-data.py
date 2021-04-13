8fdf4259d140e53f00bcf43b73b0895bd734ca54
Diogo Mera <diogo.mera@gmail.com>
2021-04-13 13:47:39 +0100

    last test

--- fetch-dssg-data.py
+++ fetch-dssg-data.py
@@ -10,12 +10,34 @@ import requests
 import io
 import datetime
 import os
+import argparse
+import os.path
+from os import path
 
 csv_url = [ "https://raw.githubusercontent.com/dssg-pt/covid19pt-data/master/data.csv",
             "https://raw.githubusercontent.com/dssg-pt/covid19pt-data/master/amostras.csv,",
             "https://raw.githubusercontent.com/dssg-pt/covid19pt-data/master/mortalidade.csv",
             "https://raw.githubusercontent.com/dssg-pt/covid19pt-data/master/vacinas.csv",
           ]
+
+parser = argparse.ArgumentParser(description='Download DSSG files')
+parser.add_argument('path', type=str, help='path to where files will be stored')
+args = parser.parse_args()
+path_args = args.path
+
+#descobrir se o path existe e simultamente que seja directório no sistema, e dar erro c.c
+if os.path.exists(path_args) is not True: 
+   print ("There is not such Folder")
+   exit(1)
+else:
+   print ("Folder:" + path_args)
+
+#descobrir se tenho permissões para escrever e caso não tenha dá erro
+if os.access(path_args, os.W_OK) is not True:
+            print("Folder not writable")
+            exit(1)
+else :
+            print("Folder writable")
 
 time_path = str(datetime.date.today())
 
@@ -30,6 +52,8 @@ for url in csv_url:
     print('saving ' + file_name_path)
     req = requests.get(url)
     url_content = req.content
-    csv_file = open(file_name_path, 'wb')
+    new_file_namepath=os.path.join(path_args, file_name_path)
+    csv_file = open(new_file_namepath, 'wb')
     csv_file.write(url_content)
     csv_file.close()
+
