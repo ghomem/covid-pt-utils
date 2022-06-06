0e62d63239c1f768282c129fd0d5509f21a9834c
Gustavo Homem <gustavo@angulosolido.pt>
2022-06-07 01:00:58 +0200

    a bit of refactoring + code style

--- merge-data.py
+++ merge-data.py
@@ -8,13 +8,13 @@ import datetime
 ### CONFIG ###
 
 INITIAL_DATE_STR = '26-02-2020'
-PATCH_DATE_STR   = '14-03-2022' # the day after the data from DSSG stopped flowing
-PATCH_DATE_STR_D = '2022-03-14' # the DSG files use a different format
+PATCH_DATE_STR   = '14-03-2022'  # the day after the data from DSSG stopped flowing
+PATCH_DATE_STR_D = '2022-03-14'  # the DSG files use a different format
 
-HOSP_PATCH_DATE_STR = '15-03-2022' # the first weekly hospitalizations record from the DSSG file
+HOSP_PATCH_DATE_STR = '15-03-2022'  # the first weekly hospitalizations record from the DSSG file
 
-INITIAL_DATE = datetime.datetime.strptime(INITIAL_DATE_STR,'%d-%m-%Y').date()
-PATCH_DATE   = datetime.datetime.strptime(PATCH_DATE_STR,'%d-%m-%Y').date()
+INITIAL_DATE = datetime.datetime.strptime(INITIAL_DATE_STR, '%d-%m-%Y').date()
+PATCH_DATE   = datetime.datetime.strptime(PATCH_DATE_STR, '%d-%m-%Y').date()
 
 # this is a very complete file from the awesome DSSG-PT group, we saved the last complete version
 # to inspect only the new cases and deaths we can use
@@ -30,7 +30,7 @@ MERGED_DATA_SUBDIR = 'merged/'
 
 ### FUNCTIONS ###
 
-def mk_dates ( initial_date, days ):
+def mk_dates( initial_date, days ):
 
     dates = []
 
@@ -44,52 +44,78 @@ def mk_dates ( initial_date, days ):
     # the current date is formatted backwards, for the file name
     return dates, cur_date.strftime('%Y-%m-%d')
 
-def print_summary ( df ):
+
+def print_summary( df ):
     print( df[ ['data', 'confirmados_novos', 'obitos'] ] )
+
+
+# gets the most recent DGS info
+def get_dgs_info( dgs_dir ):
+
+    # this is the minimalistic DSG file that started being published around 13-03-2022
+    # when the dashboard was discontinued (except for the 'amostras' part )
+
+    # columns of the DGS file
+    #
+    # * confirmation_date1 - date
+    # * day_cum_abs_num     - daily cases
+    # * day_ob_abs_num      - daily deaths
+
+    # find the latest available file
+    dgs_file_list = glob.glob(dgs_dir + '/covid_dados-*.xlsx')
+    dgs_path      = max(dgs_file_list, key=os.path.getctime)
+
+    # read the second tab of the excel file
+    dgs_data = pd.read_excel(dgs_path, sheet_name=1)
+
+    return dgs_data, dgs_path
+
+
+# gets the most recent DSSG info
+def get_dssg_info( dssg_dir ):
+
+    dssg_file_list = glob.glob(base_path + DSSG_LATEST_SUBDIR + 'data-*.csv')
+    dssg_path      = max(dssg_file_list, key=os.path.getctime)
+
+    dssg_data = pd.read_csv(dssg_path)
+
+    return dssg_data, dssg_path
+
 
 ### MAIN ###
 
-parser = argparse.ArgumentParser(description='Download DSSG files')
+parser = argparse.ArgumentParser(description='Merge DGS and DSSG files')
 parser.add_argument('path', type=str, help='path to where files will be stored')
 args = parser.parse_args()
 path_args = args.path
 
 if os.path.exists(path_args) is not True:
-   print ("There is no folder called", path_args)
-   exit(1)
+    print("There is no folder called", path_args)
+    exit(1)
 else:
-   print ("Folder:" + path_args)
+    print("Folder:" + path_args)
 
 if os.access(path_args, os.W_OK) is not True:
-            print("Folder is not writable")
-            exit(1)
+    print("Folder is not writable")
+    exit(1)
 else :
-            print("Folder is writable")
+    print("Folder is writable")
 
-# read both files
+# start the work on data files
 
 base_path = path_args + '/'
+
+# first the static DSSG file that we use for historic data
+
 dssg_path = base_path + DSSG_FILE
 dssg_data = pd.read_csv(dssg_path)
 
-# this is the minimalistic DSG file that started being published around 13-03-2022
-# when the dashboard was discontinued (except for the 'amostras' part )
-
-# columns of the DGS file
-#
-# * confirmation_date1 - date
-# * day_cum_abs_num     - daily cases
-# * day_ob_abs_num      - daily deaths
+# then the latest DGS file with daily data
 
 dgs_dir = base_path + DGS_SUBDIR
-
-# find the latest available file
-dgs_file_list = glob.glob(dgs_dir + 'covid_dados-*.xlsx')
-dgs_path  = max(dgs_file_list, key=os.path.getctime)
+dgs_data, dgs_path = get_dgs_info(dgs_dir)
 
 print('\nMerging', dssg_path, 'with', dgs_path, '\n')
-
-dgs_data  = pd.read_excel(dgs_path, sheet_name=1) # second tab
 
 # we now wish to patch the DSSG file using the data from the DGS file starting at PATCH_DATE
 
@@ -103,7 +129,7 @@ cases  = dgs_data_tail['day_cum_abs_num']
 deaths = dgs_data_tail['day_ob_abs_num' ]
 ndays  = len(cases.index)
 
-dates, last_date  = mk_dates(PATCH_DATE, ndays)
+dates, last_date = mk_dates(PATCH_DATE, ndays)
 
 # the deaths need to be cumulative, and the last value from DSSG needs to be added
 
@@ -111,20 +137,18 @@ cum_deaths = np.cumsum(deaths) + dssg_data.iloc[-1]['obitos']
 
 # let's create a dataframe with the DSSG naming, the remaining columns are filled with NaN
 
-extra_dssg_data = pd.DataFrame( {'data':dates, 'confirmados_novos':cases.to_list(), 'obitos':cum_deaths.astype(int).to_list() } )
+extra_dssg_data = pd.DataFrame( {'data': dates, 'confirmados_novos': cases.to_list(), 'obitos': cum_deaths.astype(int).to_list() } )
 
 # and finally let's merge the dataframes
 
-merged_dssg_data = dssg_data.append(extra_dssg_data, ignore_index = True, sort = False)
+merged_dssg_data = dssg_data.append(extra_dssg_data, ignore_index=True, sort=False)
 
 print_summary(merged_dssg_data)
 
 # now let's look for the latest dssg file to extract the hospitalizations data
 
-dssg_file_list = glob.glob(base_path + DSSG_LATEST_SUBDIR + 'data-*.csv')
-dssg_latest_path = max(dssg_file_list, key=os.path.getctime)
-
-dssg_latest_data = pd.read_csv(dssg_latest_path)
+dssg_dir = base_path + DSSG_LATEST_SUBDIR
+dssg_latest_data, dssg_latest_path = get_dssg_info(dssg_dir)
 
 index = dssg_latest_data.loc[ dssg_latest_data['data'] == HOSP_PATCH_DATE_STR ].index[0]
 
@@ -161,4 +185,3 @@ print('\nSaving CSV at', merged_path)
 merged_dssg_data.to_csv(merged_path, index=False)
 
 print('\nInspect with:\n', 'csvtool -t \',\' col 1,12,14,15,16', merged_path, '|csvtool -t \',\' readable - |less' )
-
